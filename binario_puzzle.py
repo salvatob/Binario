6046b46b6fda46eff29c0b81b9a792bcafb329b2
Tobiáš Salva <tobias@avlas.cz>
2024-11-16 19:38:18 +0100

    Change result printing in binario_puzzle.py

--- binario_puzzle.py
+++ binario_puzzle.py
@@ -35,7 +35,7 @@ def write_cnf(clauses : list[str], num_of_variables: int, output_file_path: str)
         for clause in clauses:
             file.write(f"\n{clause} 0")
 
-def call_solver(input_file_path: str, solver_architecture : str,verbosity: int = 2) -> str:
+def call_solver(input_file_path: str, solver_architecture : str, verbosity: int = 2) -> str:
     solver_path = ""
     if solver_architecture == "win":
         solver_path = "win_solver/glucose-syrup.exe"
@@ -51,30 +51,39 @@ def call_solver(input_file_path: str, solver_architecture : str,verbosity: int =
     for line in solver_result.stdout.decode('utf-8').split('\n'):
         if line.startswith('v'):
             result_line = line
-        print(line)
+        if verbosity > 0:
+            print(line)
 
     return result_line
 
 def human_readable_result(result: str):
+    if result == "":
+        print("Puzzle is unsolvable")
+        return
+
     result = result[1:-2].strip().split()
     line_size = math.floor(math.sqrt(len(result)))
 
+    print("-"*(line_size*4+1))
     for i in range(line_size):
         line = ""
         for j in range(line_size):
             if result[i * line_size+j].startswith("-"):
-                line += "O "
+                line += "| O "
             else:
-                line += "X "
-        print(line)
+                line += "| X "
+        print(line + "|")
+        print("-"*(line_size*4+1))
+
+
 
 
 def main(args):
     encode_puzzle(args.input, args.output, args.verb)
 
     result = call_solver(args.output,args.system, args.verb)
-    if args.verb > 0:
-        human_readable_result(result)
+
+    human_readable_result(result)
 
 
 if __name__ == '__main__':
